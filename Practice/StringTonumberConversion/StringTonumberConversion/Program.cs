4f4e148b8090e2023940289ffe3b4115eaa458e3
VishalB96 <vsb12vsb12@gmail.com>
2020-10-28 23:14:18 +0530

    practice minor update

--- Practice/StringTonumberConversion/StringTonumberConversion/Program.cs
+++ Practice/StringTonumberConversion/StringTonumberConversion/Program.cs
@@ -7,7 +7,7 @@ namespace StringTonumberConversion
     {
         static void Main(string[] args)
         {
-            string s1 = "45";
+           /* string s1 = "45";
 
             int i1 = int.Parse(s1);
             int i2 = Convert.ToInt32(s1);
@@ -17,21 +17,26 @@ namespace StringTonumberConversion
             float f3;
 
             bool b1 = int.TryParse(s1,out i3);
-            bool b2 = float.TryParse(s1, out f3);
+            bool b2 = float.TryParse(s1, out f3);*/
             int i4;
             if (int.TryParse(Console.ReadLine(), out i4))
+            {
                 Console.WriteLine("converted ");
+            }
             else
-                Console.WriteLine("Not converted");
+            {
+
+                Console.WriteLine("Not converted {0}",i4);
+            }
             
-            checked {
+           /* checked {
                 string s2 = null;
                 //int i5 = int.Parse(s2);
                 int i5 = Convert.ToInt32(s2);
                 Console.WriteLine(s1);
                 Console.WriteLine(i5);
                 Console.ReadLine();
-            }
+            }*/
 
 
         }
